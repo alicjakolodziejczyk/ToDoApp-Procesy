2f312db33e671564d84d3a795ea050a59f14f2b1
alicjakolodziejczyk <alicja.kolodziejczyk.01@gmail.com>
2023-12-10 22:15:36 +0100

    added tests for heading and input

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -7,11 +7,14 @@ import Home from '@/pages/index'
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
-
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
-
-    expect(heading).toBeInTheDocument()
+    const heading = screen.getByRole("heading", { name: /Todo App/i });
+    expect(heading).toBeInTheDocument();
   })
+
+  it("renders an input", () => {
+    render(<Home />)
+    const input = screen.getByPlaceholderText(/Your todo here/i);
+    expect(input).toBeInTheDocument();
+  })
+
 })
