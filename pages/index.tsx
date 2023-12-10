56f504703dd0d653b42ae03c042b34ecbf8cc382
alicjakolodziejczyk <alicja.kolodziejczyk.01@gmail.com>
2023-12-10 22:35:50 +0100

    Improved styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 export default function index() {
   return (
     <div className='flex flex-col justify-start items-center h-screen'>
-      <h1 className='font-bold m-5'>Todo App</h1>
-      <input className='border-2 border-gray-400 rounded-md p-2 w-1/2' type='text' placeholder='Your todo here' />
-      <button className='border-2 border-gray-400 rounded-md p-2 w-1/2 mt-2'>Add</button>
+      <h1 className='text-3xl font-bold m-8'>Todo App</h1>
+      <div className='flex flex-row w-1/2'>
+        <input className='border-2 border-gray-400 rounded-md p-2 w-full' type='text' placeholder='Your todo here' />
+        <button className='border-2 border-gray-400 rounded-md p-2 w-16 mx-2'>Add</button>
+      </div>
+      
     </div>
   )
 }
